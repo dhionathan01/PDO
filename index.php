f216c5288f1747a66604ea5906d7d0f23e1b93b3
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-07-22 14:45:57 -0300

    Tratando exceções com o PDOException

--- index.php
+++ index.php
@@ -8,10 +8,17 @@
      - e informamos o nome e senha do banco em questão
      */
 
-     $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
+     $dsn = 'mysql:host=localhost;dbname=xphp_com_pdo';
      $usuario = 'root';
      $senha = '';
-    $conexao = new PDO($dsn, $usuario, $senha);
+     try{
+        $conexao = new PDO($dsn, $usuario, $senha);
+
+     }catch(PDOException $e){
+        echo 'Erro: '.$e->getCode(). ' Mensagem: '.$e->getMessage() ; // Utilizandos o methodos do PDOException para extrair o erro
+        
+     }
+    
 
 
 ?>
