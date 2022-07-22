75c92fa9ff7a8d8197cc609c5045cc49e1b58d6f
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-07-22 15:20:10 -0300

    Executando instruções SQL no código PHP

--- index.php
+++ index.php
@@ -8,12 +8,45 @@
      - e informamos o nome e senha do banco em questão
      */
 
-     $dsn = 'mysql:host=localhost;dbname=xphp_com_pdo';
+     $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
      $usuario = 'root';
      $senha = '';
      try{
         $conexao = new PDO($dsn, $usuario, $senha);
+        // Executando  intruções SQL:
+        // Comandos sql:
+        // Criando a tabela tb_usuarios
+        $query = '
+            CREATE TABLE if not exists tb_usuarios(
+                id int not null primary key auto_increment,
+                nome varchar(50) not null,
+                email varchar(100) not null,
+                senha varchar(32) not null
+            )
+        ';
 
+       // executa o conteúdo da variavel query no respectivo banco de dados informado na variável conexão 
+        $retorno = $conexao->exec($query);
+        echo $retorno;
+
+        // Inserindo um usuario
+        $query = '
+            insert into tb_usuarios(
+                nome, email, senha
+            ) values(
+                "Dhionathan Lanzoni Jobim", "dhionathan01@hotmail.com", "000000"
+            )
+        ';
+        
+        $retorno = $conexao->exec($query);
+        echo $retorno;
+        // Deletando usuarios:
+       /*  $query = '
+        delete from tb_usuarios
+    '; */
+    
+    $retorno = $conexao->exec($query);
+    echo $retorno;
      }catch(PDOException $e){
         echo 'Erro: '.$e->getCode(). ' Mensagem: '.$e->getMessage() ; // Utilizandos o methodos do PDOException para extrair o erro
         
